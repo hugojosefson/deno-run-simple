346714ccc4e6cad56d975818efd143288fc9864c
Hugo Josefson <hugo@josefson.org>
2022-07-22 15:28:35 +0200

    refactor: specify types

--- src/os.ts
+++ src/os.ts
@@ -1,8 +1,9 @@
 export async function weakEnvGet(
   variable: string,
 ): Promise<false | string | undefined> {
-  const query = { name: "env" as const, variable };
-  const permissionResponse = await Deno.permissions.query(query);
-  const isGranted = permissionResponse.state === "granted";
+  const query: Deno.PermissionDescriptor = { name: "env" as const, variable };
+  const permissionResponse: Deno.PermissionStatus = await Deno.permissions
+    .query(query);
+  const isGranted: boolean = permissionResponse.state === "granted";
   return isGranted && Deno.env.get(variable);
 }
