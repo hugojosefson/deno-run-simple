352ea79c58f38985c1ab3981201088058160c396
Hugo Josefson <hugo@josefson.org>
2023-02-12 17:17:22 +0100

    test: add tests for failing run

--- test/run.test.ts
+++ test/run.test.ts
@@ -5,3 +5,23 @@ Deno.test("run: echo hello", async () => {
   const result = await run("echo hello");
   assertEquals(result, "hello");
 });
+Deno.test("run: false -> rejects", async () => {
+  let didReject: boolean | undefined;
+  try {
+    await run("false");
+    didReject = false;
+  } catch (_expected) {
+    didReject = true;
+  }
+  assertEquals(didReject, true);
+});
+Deno.test("run: false -> rejects with an Error", async () => {
+  let didRejectWithError: boolean | undefined;
+  try {
+    await run("false");
+    didRejectWithError = false;
+  } catch (e) {
+    didRejectWithError = e instanceof Error;
+  }
+  assertEquals(didRejectWithError, true);
+});
