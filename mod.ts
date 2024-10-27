5588d23495e88968cf61858b8b1f3c91bf35e5fd
Hugo Josefson <hugo@josefson.org>
2024-10-27 11:06:19 +0100

    refactor: sort exports in order of relevance

--- mod.ts
+++ mod.ts
@@ -1,2 +1,7 @@
-export type { CommandFailure, RunOptions, SimpleValue } from "./src/run.ts";
-export { jsonArrayRun, jsonlRun, jsonRun, run } from "./src/run.ts";
+export { run } from "./src/run.ts";
+export type { RunOptions } from "./src/run.ts";
+export type { CommandFailure } from "./src/run.ts";
+export type { SimpleValue } from "./src/run.ts";
+export { jsonRun } from "./src/run.ts";
+export { jsonArrayRun } from "./src/run.ts";
+export { jsonlRun } from "./src/run.ts";
