869c9cadf86425fabb2258bdd3abb06b6fdd8c1b
Hugo Josefson <hugo@josefson.org>
2025-04-25 14:39:55 +0200

    feat: export CommandFailureError

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
 export { run } from "./src/run.ts";
 export type { RunOptions } from "./src/run.ts";
-export type { CommandFailure } from "./src/run.ts";
+export { type CommandFailure, CommandFailureError } from "./src/run.ts";
 export type { SimpleValue } from "./src/run.ts";
 export { jsonRun } from "./src/run.ts";
 export { jsonArrayRun } from "./src/run.ts";
